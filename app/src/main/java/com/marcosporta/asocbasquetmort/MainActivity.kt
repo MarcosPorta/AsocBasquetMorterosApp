f6ac9eba6a23d5cc9e5a8e9b3337917c36e5046f
MarcosPorta <marcosporta15@gmail.com>
2022-05-11 17:22:31 -0300

    Eliminacion de Toast's y lista version 2.1

--- app/src/main/java/com/marcosporta/asocbasquetmort/MainActivity.kt
+++ app/src/main/java/com/marcosporta/asocbasquetmort/MainActivity.kt
@@ -24,22 +24,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickFixture(view: View){
-        var intent= Intent(this,FixtureActivity::class.java)
+        val intent= Intent(this,FixtureActivity::class.java)
         startActivity(intent)
-        //toast
-        Toast.makeText(this,"Fixture",Toast.LENGTH_LONG).show()
     }
     fun clickPosiciones(view: View){
-        var intent= Intent(this,PosicionesActivity::class.java)
+        val intent= Intent(this,PosicionesActivity::class.java)
         startActivity(intent)
-        //toast
-        Toast.makeText(this,"Posiciones",Toast.LENGTH_LONG).show()
     }
     fun clickEstadisticas(view: View){
-        var intent= Intent(this,EstadisticasActivity::class.java)
+        val intent= Intent(this,EstadisticasActivity::class.java)
         startActivity(intent)
-        //toast
-        Toast.makeText(this,"Estadisticas",Toast.LENGTH_LONG).show()
     }
 
 }
